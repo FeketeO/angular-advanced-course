8b1a7f7ad1dff247eab5af7dbde9324d9535c89b
FeketeO <69634719+FeketeO@users.noreply.github.com>
2021-08-07 21:53:50 +0200

    auth service - 9 hiba és nem értem a tap-től

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
 import { User } from '../model/user';
 import { ConfigService } from './config.service';
+import { UserService } from './user.service';
 
 
 @Injectable({
@@ -15,6 +17,7 @@ export class AuthService {
   logoutnUrl = `${this.config.apiUrl}logout`;
   currentUserSubject: BehaviorSubject<User> = new BehaviorSubject(null);
   lastToken: string = '';
+  storageName = 'currentUser';
 
 
 
@@ -22,6 +25,7 @@ export class AuthService {
     private config: ConfigService,
     private http: HttpClient,
     private router: Router,
+    private userService: UserService,
   ) { }
 
   //Írunk egy függvényt, amely kiolvassa a user pillanatnyi értékét.
@@ -29,6 +33,32 @@ export class AuthService {
     return this.currentUserSubject.value;
   }
 
+  //login
+  login(loginData: User): Observable<{ accessToken: string }> {
+    return this.http.post<{ accessToken: string }>(
+      this.loginUrl,
+      { email: loginData.email, password: loginData.password }
+    )
+    .pipe( switchMap( response => {
+      if (response.accessToken) {
+        this.lastToken = response.accessToken;
+        return this.userService.query(`email=${loginData.email}`);
+      }
+      return of(null);
+    }))
+    .pipe(
+      tap( user => {
+        if (!user) {
+          localStorage.removeItem(this.storageName);
+          this.currentUserSubject.next(null);
+        } else {
+          user[0].token = this.lastToken;
+          localStorage.setItem(this.storageName, JSON.stringify(user[0]));
+          this.currentUserSubject.next(user[0]);
+        }
+      })
+    );
+  }
 //Végül kell egy router, amely segítségével elkészítjük a logout függvényt:
   logout() {
     localStorage.removeItem('currentUser');
